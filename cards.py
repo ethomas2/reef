75c4e07a36e309dd8f18454dc06a1d2388493737
Evan Thomas <evan@tempoautomation.com>
2019-09-30 09:24:14 -0700

    Add cards

---
+++ cards.py
@@ -0,0 +1,72 @@
+# Square
+# ThreeL
+# AnyStack
+# ThreeStack/FourStack//TwoStack
+# TwoPlusStack
+# ThreeOrthog
+# TwoOrthog
+# ThreeDiag or TwoDiag
+# HighestSurround(highest, surrounder)
+
+
+purple, purple, Square(red), 6vp
+green, green, ThreeStack(yellow), 4vp
+red, yellow, FourStack(purple),  5vp
+purple, purple, ThreeDiag(red), 4vp
+purple, purple, ThreeStack(green), 4vp
+yellow, green, ThreeStack(rainbow), 2vp
+red, red, AnyStack(green), 1vp
+yellow, yellow, Square(green), 6vp
+green, green, TwoOrthog(AnyStack(purple), AnyStack(yellow)), 3vp
+green, green, HighestSurround(purple, yellow), 2vp
+red, red, TwoStack(yellow), 2vp
+green, green, ThreeL(yellow), 4vp
+yellow, yellow, ThreeDiag(green), 4vp
+yellow, yellow, AnyStack(purple), 1vp
+yellow, yellow, AnyStack(purple), 1vp
+red, yellow, ThreeOrthog(green), 4vp
+green, green, TwoOrthog(TwoStack(purple), TwoStack(purple)), 5vp
+purple, purple, ThreeL(green), 4vp
+red, red, ThreeL(purple), 4vp
+red, red, HighestSurround(yellow, purple), 2vp
+purple, purple, AnyStack(yellow), 1vp
+purple, red, FourStack(rainbow), 3vp
+yellow, purple, FourStack(green), 5vp
+red, purple, TwoStack(rainbow), 1vp
+green, green, TwoDiag(TwoPlus(purple), TwoPlus(red)), 5vp
+purple, purple, TwoOrthog(TwoStack(red), TwoStack(red)), 5vp
+green, green, TwoOrthog(AnyStack(red), AnyStack(purple)), 3vp
+red, red, Square(yellow), 6vp
+green, green, TwoStack(purple), 2vp
+red, red, ThreeStack(purple), 4vp
+purple, purple, HighestSurround(red, green), 2vp
+yellow, yellow, HighestSurround(green, red), 2vp
+green, green, ThreeDiag(purple), 4vp
+red, green, ThreeOrthog(purple), 4vp
+red, red, HigestSurround(purple, green), 2vp
+green, purple, FourStack(red), 5vp
+green, yellow, TwoOrthog(TwoStack(rainbow), TwoStack(rainbow)), 2vp
+yellow, yellow, TwoStack(green), 2vp
+red, red, TwoDiag(TwoPlusStack(purple), TwoPlusStack(yellow)), 5vp
+green, green, HigestSurround(yellow, red), 2vp
+purple, purple, TwoOrthog(AnyStack(red), AnyStack(green)), 3vp
+green, green, AnyStack(red), 1vp
+red, red, ThreeDiag(yellow), 4vp
+yellow, yellow, TwoOrthog(TwoStack(green), TwoStack(green)), 5vp
+purple, purple, TwoDiag(TwoPlusStack(red), TwoPlusStack(yellow)), 5vp
+yellow, yellow, ThreeStack(red), 4vp
+yellow, yellow, TwoDIag(TwoPlusStack(red), TwoPlusStack(green)), 5vp
+red, red, TwoDiag(TwoPlusStack(yellow), TwoPlusStack(green)), 5vp
+red, red, TwoOrthog(AnyStack(green), AnyStack(yellow)), 3vp
+yellow, yellow, TwoDiag(TwoPLusStack(purple), TwoPlusStack(green)), 5vp
+purple, purple, HighestSurround(green, yellow), 2vp
+green, green, Square(purple), 6vp
+green, purple, ThreeOrthog(yellow), 4vp
+purple, purple, TwoStack(red), 2vp
+red, red, TwoOrthog(TwoStack(yellow), TwoStack(yellow)), 5vp
+yellow, yellow, ThreeL(red), 4vp
+red, green, FourStack(yellow), 5vp
+purple, yellow, ThreeOrthog(red), 4vp
+yellow, yellow, HighestSurround(red, purple), 2vp
+purple, purple, TwoOrthog(AnyStack(yellow), AnyStack(red)), 3vp
+yellow, yellow, TwoOrthog(AnyStack(purple), AnyStack(green)), 3vp

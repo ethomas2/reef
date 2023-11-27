36eb4cfaf2ca2adf6a9a6392f6fd319de1436d05
Evan Thomas <ethomas@hiddenroad.com>
2023-11-26 21:12:00 -0800

    Add cli parser

--- rust/t2048/src/main.rs
+++ rust/t2048/src/main.rs
@@ -1,7 +1,24 @@
+use clap::{Parser, ValueEnum};
 use std::error::Error;
+use std::str::FromStr;
 use t2048::rules;
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum PlayerType {
+    Random,
+    Mcts,
+    Human,
+    Minimax,
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[arg(short, long, value_enum)]
+    player_type: PlayerType,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
     println!("foobar");
     rules::stroopwaffle();
     Ok(())
